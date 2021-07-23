049dd76e245ac916b0e4af146f083aa27cb2e409
April Day <april.day@gmail.com>
2021-07-23 03:34:33 +0100

    add code to about/section1 commit

---
+++ src/components/About/section1.vue
@@ -0,0 +1,217 @@
+<template>
+    <div>
+        <div class="body">
+            <div class="container px-0">
+                <div class="row">
+                    <!-- details holder here -->
+                    <div class="col-md-8">
+                        <div class="row white_bg">
+                         <!--image/screenshot here -->
+                        <div class="col-md-6 img_holder">
+                            <div> 
+                                <img
+                                    :src="${companies.screenshot}"
+                                    class="img-fluid"
+                                    alt 
+                                />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="details">
+                                <h4>
+                                    {{ companies? companies.title_ : 'Not available'}}
+                                </h4>
+                                <h5 class="pt-3">
+                                    headquarters: 
+                                    <span>
+                                        {{
+                                            companies? companies.headquarters: 'Not available'
+                                        }}
+                                    </span>
+                                </h5>
+                                <h5 class="pt-3">
+                                    industry: 
+                                    <span>
+                                        {{
+                                            companies? companies.industry: 'Not available'
+                                        }}
+                                    </span>
+                                </h5>
+                                <h5 class="pt-3">
+                                    address: 
+                                    <span>
+                                        {{
+                                            companies? companies.companyAddress: 'Not available'
+                                        }}
+                                    </span>
+                                </h5>
+                                <h5 class="pt-3">
+                                    founded: 
+                                    <span>
+                                        {{
+                                            companies? companies.founded: 'Not available'
+                                        }}
+                                    </span>
+                                </h5>
+                                <div class="inline pt-3">
+                                    <p>
+                                        Rating: 
+                                        {{ companies ? companies.rating: 'Not available'}}
+                                    </p>
+                                    <p>
+                                        Price: 
+                                        {{ companies ? companies.price: 'Not available'}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <!-- mini side bar here -->
+                    <div class="col-md-4 white__bg">
+              <h4>Company Info</h4>
+              <div>
+                <img :src="`${companies.logo}`" alt />
+              </div>
+              <div>
+                <span class="badge badge-pill mt-3 badge-success">
+                  {{ companies ? companies.type : 'Not available' }}
+                </span>
+              </div>
+              <div>
+                <h6 class="mt-3">
+                  {{ companies ? companies.company_name : 'Not available' }}
+                </h6>
+              </div>
+              <div>
+                <h6 class="mt-3">
+                  {{ companies ? companies.company_size : 'Not available' }}
+                </h6>
+              </div>
+              <div>
+                <a :href="'' + companies.website_" target="_blank">
+                  {{ companies ? companies.website_ : 'Not available' }}
+                </a>
+              </div>
+              <div class="social__holder mt-3">
+                <a :href="'' + companies.facebookUrl" target="_blank">
+                <i class="fa fa-facebook mx-2"></i>
+                </a>
+                <a :href="'' + companies.twitterUrl" target="_blank">
+                <i class="fa fa-twitter mx-2"></i>
+                </a>
+                <a :href="'' + companies.linkedinUrl" target="_blank">
+                <i class="fa fa-linkedin mx-2"></i>
+                </a>
+                <a :href="'mailto:' + companies.email" target="_blank">
+                 <i class="fa fa-envelope mx-2"></i>
+                </a>
+                </div>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+
+// following auto added in Vue 3
+/*
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+*/
+
+    const axios = require('axios')
+    export default {
+        name: '',
+        data() {
+            return {
+                secretKey: '$2b$10$Wb8VppNIDoAhIAB8n8oFnuCuOnANGbL7pxknMH8lhYplG40/hi5LC',
+                companies: null,
+                router: this.$route.params.companyid,
+            }
+        },
+        mounted() {
+            this.getCompany()
+        },
+        methods: {
+            async getCompany() {
+                try {
+                    let response = await this.$http.get(
+                      `https://api.jsonbin.io/b/5f20829bc58dc34bf5dca275`,
+                    {
+                        headers: {
+                            'secretKey' : this.secretKey, 
+                        },
+                    },
+                    )
+
+                    console.log(response.data)
+                    this.companies = response.data.find(
+                        (company) => company.objectID === this.router,
+                    )
+                    console.log(this.companies)
+                } catch (error) {
+                    console.log("there was an error: ", error.response)
+                }
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    @import url(//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css);
+    .body {
+        background: #f6f8fa;
+        height: 100%;
+    }
+    .white_bg{
+        background: white;
+        border: 2px solid white;
+        width: 100%;
+        box-shadow: 0 0.75rem 6rem rgba(56, 65, 74, 0.03);
+        margin-bottom: 24px;
+        border-radius: 0.25rem;
+        padding: 2rem;
+        margin-top: 20px;
+    }
+    .details {
+        text-align: left;
+    }
+    h3 {
+        font-size: 22px;
+        margin-top: 20px;
+    }
+    h4 {
+        font-size: 20px;
+    }
+    h5 {
+        font-size: 16px;
+    }
+    .pt-3 span {
+        font-size: 14px;
+        line-height: 22px;
+    }
+    a {
+        text-decoration: none !important;
+    }
+    .fa {
+        color: black;
+    }
+    path {
+        color: black;
+        height: 4vh;
+    }
+    path :hover {
+        color: blue;
+    }
+
+</style>
