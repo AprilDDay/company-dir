3dde3760b902c5dc693acfd0ec4c966250a87569
April Day <april.day@gmail.com>
2021-07-24 15:07:21 +0100

    add styled scoped to about/section2 commit

---
+++ src/components/About/section2.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    .body {
+        background: #f6f8f8;
+        height: 100%;
+    }
+
+    .white_bg {
+        background: white;
+        border: 2px solid white;
+        width: 100%;
+        box-shadow: 0 0.75rem 6rem rgba(56, 65, 74, 0.03);
+        margin-bottom: 24px;
+        border-radius: 0.25rem;
+        padding: 2rem;
+        margin-top: 20px;
+    }
+
+    .about p {
+        text-align: left;
+        font-size: 14px;
+        line-height: 25px;
+    }
+</style>
